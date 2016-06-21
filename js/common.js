d687a4bf984319a7fbee2144b37307b6a293e756
Nasirov-T <this-is-my-mail@mail.ru>
2016-06-21 17:50:30 +0500

    test page
    test1

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+$('.folder').on('mouseenter', function(){
+	var tl = new TweenMax();
+
+	TweenMax.to(".img1", 1, {left: 100, scale: 2, ease: Bounce}, 'icon');
+
+
+
+
+})
+
+
+  }); 
